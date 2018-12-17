35da9d4447b3aba32987a690bc1bac1bcd9d6362
mmayolini <maxime.mayolini@gmail.com>
2018-12-17 11:19:30 +0100

    initial commit

---
+++ spark_maven/src/main/java/bigdata/SparkStreaming.java
@@ -0,0 +1,43 @@
+package bigdata;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.*;
+import org.apache.spark.api.java.function.*;
+import org.apache.spark.streaming.*;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.twitter.TwitterUtils;
+
+import twitter4j.HashtagEntity;
+import twitter4j.Status;
+import twitter4j.auth.Authorization;
+import twitter4j.auth.AuthorizationFactory;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationContext;
+
+public class SparkStreaming {
+
+	public static void main(String[] args) {
+		
+		SparkConf sparkConf = new SparkConf()
+		        .setAppName("SparkStreaming")
+		        .setMaster("local[2]");
+		JavaStreamingContext sc = new JavaStreamingContext(sparkConf, new Duration(5000));
+				
+		Configuration twitterConf = ConfigurationContext.getInstance();
+		Authorization twitterAuth = AuthorizationFactory.getInstance(twitterConf);
+		
+		String[] filters = { "#Android" };
+		TwitterUtils.createStream(sc, twitterAuth, filters)
+        .flatMap(s -> Arrays.asList(s.getHashtagEntities()))
+        .map(h -> h.getText().toLowerCase())
+        .filter(h -> !h.equals("android"))
+        .countByValue()
+        .print();
+		
+		sc.start();
+		sc.awaitTermination();
+	}
+	
+}
